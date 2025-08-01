e3966bbe05729ed9906c023a1a9424290027bd63
yuuki798 <b19357160114@gmail.com>
2025-08-01 16:26:05 +0800

    doc
    Signed-off-by: yuuki798 <b19357160114@gmail.com>

--- dev/initial/config.go
+++ dev/initial/config.go
@@ -9,8 +9,13 @@ import (
 func InitConfig() {
 	// 设置命令行参数
 	var configFile string
-	flag.StringVar(&configFile, "config", "./config/config.dev.yaml", "配置文件路径")
+	flag.StringVar(&configFile, "config", "./config.dev.yaml", "配置文件路径")
 	flag.Parse()
+	if configFile == "config.dev.yaml" {
+		log.Println("使用默认配置文件: config.dev.yaml")
+	} else {
+		log.Printf("使用配置文件: %s", configFile)
+	}
 	// 设置viper配置
 	viper.SetConfigFile(configFile)
 	// 设置配置文件类型
